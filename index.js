91f3def6885cc058c4634a953d5b00687ca65bba
taufiq72 <taufiqq1207@gmail.com>
2026-07-20 11:23:33 +0700

    menambahkan app.delete

--- index.js
+++ index.js
@@ -55,3 +55,17 @@ app.put('/komik/:id', async (req, res) => {
         res.status(500).send(error);
     }
 });
+
+app.delete('/komik/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+        await komik.destroy();
+        res.send({ message: 'komik berhasil dihapus', komik });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
